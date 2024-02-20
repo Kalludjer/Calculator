266510b67fd1097b4f66d05ebe7a45a7b6c19907
Kalludjer <necicdragan3@GMAIL.COM>
2024-02-20 16:39:28 +0100

    add java script

--- main.js
+++ main.js
@@ -1,12 +1,92 @@
-function display (val){
-    document.getElementById("result").value += val;
-    return val
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal-sign");
+    let decimal = document.querySelector(".dot");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current")
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+         currentScreen.textContent = currentValue;
+    }))
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + "" + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = "";
+        currentValue = "";
+        operator = "";
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    equal.addEventListener("click", function(){
+        if(currentValue != '' && previousValue != ''){
+            calculate()
+            previousScreen.textContent = "";
+            if(previousValue.length <= 5){
+                currentScreen.textContent = previousValue;
+            } else {
+                currentScreen.textContent = previousValue.slice(0.5) + "..."
+            }
+        }
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+    })
+})
+
+function handleNumber(num){
+    if(currentValue.length <=8){
+        currentValue += num;
+    }
 }
 
-function solve(){
-    let x = document.getElementById("result").value
-    let y = eval(x);
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = ""
+}
 
-    document.getElementById("result").value = y
-    return y;
+function calculate (){
+    previousValue = Number(previousValue);
+    cuirrentValue = Number(currentValue);
+
+    if (operator === "+") {
+        previousValue += currentValue;
+    } else if (operator === "-"){
+        previousValue -= currentValue;
+    } else if ( operator === "*"){
+        previousValue *= currentValue;
+    } else {
+        previousValue / currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if (!currentValue.includes(".")){
+        currentValue += '.';
+    }
 }
